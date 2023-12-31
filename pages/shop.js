cc2122267af971a2fecb0e154cb8cecd1f22210d
maazmasood <84343122+maazmasood@users.noreply.github.com>
2024-01-01 03:07:20 +0500

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -10,6 +10,12 @@ const ProductSearch = ({ onProductClick }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedProductId, setSelectedProductId] = useState(null);
 
+  const [isFilterCollapsed, setFilterCollapsed] = useState(true);
+
+ const toggleFilter = () => {
+   setFilterCollapsed(!isFilterCollapsed);
+ };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -106,14 +112,26 @@ const ProductSearch = ({ onProductClick }) => {
   };
 
   return (
+
     <div className="flex">
+    <button
+        className="md:hidden fixed top-28 left-4 p-2 bg-indigo-600 text-white rounded-full "
+        onClick={toggleFilter}
+      >
+        {isFilterCollapsed ? "Show Filters" : "Hide Filters"}
+      </button>
+
+
+
       {/* Filters */}
       <div
-        className="w-1/5 bg-gray-100 p-4 overflow-y-auto"
-        style={{ minWidth: "150px" }}
-      >
+       className={`min-w-[200px] md:w-1/5 bg-gray-100 p-4 overflow-y-auto  ${
+         isFilterCollapsed ? "hidden md:block" : "md:block"
+       }`}
+     >
         <div className="mb-8 text-center">
-          <h1 className="text-4xl font-bold text-indigo-700 mb-2">
+          <h1 className="text-4xl hidden md:block font-bold text-indigo-700 mb-2" >
+
             🌫️<span className="hidden lg:inline">Klimaanlagen</span>
           </h1>
         </div>
@@ -186,78 +204,70 @@ const ProductSearch = ({ onProductClick }) => {
           ))}
         </div>
       </div>
-      <div className="w-3/4 p-4 overflow-y-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-          {paginatedProducts.map((product) => (
-            <div
-              key={product.id}
-              className="bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl transition duration-300 cursor-pointer"
-              onClick={() => handleProductClick(product.id)}
-            >
-              <div
-                className="bg-cover bg-center h-56 p-4"
-                style={{
-                  backgroundImage: `url(${
-                    Array.isArray(product.image)
-                      ? product.image[0]
-                      : product.image
-                  })`,
-                }}
-              ></div>
-              <div className="p-4">
-                <p className="uppercase tracking-wide text-xs text-gray-700">
-                  {product.category}
-                </p>
 
-                <p className="text-gray-700 font-bold">{product.name}</p>
-                <p className="text-3xl text-right text-gray-900">{`$${product.price.toFixed(
-                  2
-                )}`}</p>
-              </div>
-              <div className="flex p-4 border-t border-gray-300 text-gray-700">
-                <div className="flex-1 inline-flex items-center">
-                  <svg
-                    className="h-6 w-6 text-gray-600 fill-current mr-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
+
+      <div className="w-full md:w-3/4 p-4">
+              {/* Flex layout instead of grid */}
+            <div className="mb-4 flex flex-wrap">
+                {paginatedProducts.map((product) => (
+                  <div
+                    key={product.id}
+                    className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3 px-4 mb-4 border cursor-pointer"
+                    onClick={() => handleProductClick(product.id)}
                   >
-                    <path d="M19 5h1V1H4v4h1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h2V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1zm0 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V6h-2v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6H3.76L1.04 16h21.92L20.24 6H19zM1 17v4h22v-4H1zM6 4v4h4V4H6zm8 0v4h4V4h-4z"></path>
-                  </svg>
-                  <p>
-                    <span className="text-gray-900 font-bold">
-                      {product.room}
-                    </span>{" "}
-                  </p>
-                </div>
-                <div className="flex-1 inline-flex items-center">
-                  <svg
-                    className="h-6 w-6 text-gray-600 fill-current mr-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  ></svg>
-                  <p>
-                    <span className="pl-1 text-gray-900 font-bold">
-                      {product.wifi}
-                    </span>{" "}
-                  </p>
-                </div>
+                    <div
+                      className="bg-cover mt-2 bg-center h-56 p-4"
+                      style={{
+                        backgroundImage: `url(${
+                          Array.isArray(product.image)
+                            ? product.image[0]
+                            : product.image
+                        })`,
+                      }}
+                    ></div>
+                    <div className="p-4">
+                      <p className="uppercase tracking-wide text-xs text-gray-700">
+                        {product.category}
+                      </p>
 
-                <div className="flex-1 inline-flex items-center">
-                  <svg
-                    className="h-6 w-6 text-gray-600 fill-current mr-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  ></svg>
-                  <p>
-                    <span className="text-black pl-1 text-l font-bold">
-                      {product.brand}
-                    </span>{" "}
-                  </p>
-                </div>
+                      <p className="text-gray-700 font-bold">{product.name}</p>
+                      <p className="text-3xl text-right text-gray-900">{`$${product.price.toFixed(
+                        2
+                      )}`}</p>
+                    </div>
+                    <div className="flex p-4 border-t border-gray-300 text-gray-700">
+                      <div className="flex-1 inline-flex items-center">
+                        <svg
+                          className="h-6 w-6 text-gray-600 fill-current mr-3"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                        <path d="M19 5h1V1H4v4h1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h2V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1zm0 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V6h-2v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6H3.76L1.04 16h21.92L20.24 6H19zM1 17v4h22v-4H1zM6 4v4h4V4H6zm8 0v4h4V4h-4z"></path>
+
+                        </svg>
+                        <p>
+                          <span className="text-gray-900 font-bold">
+                            {product.room}
+                          </span>{" "}
+                        </p>
+                      </div>
+
+                      <div className="flex-1 inline-flex items-center">
+                        <svg
+                          className="h-6 w-6 text-gray-600 fill-current mr-3"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        ></svg>
+                        <p>
+                          <span className="text-black pl-1 text-l font-bold">
+                            {product.brand}
+                          </span>{" "}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          ))}
-        </div>
 
         {totalPages > 1 && (
           <div className="flex justify-center mt-4">
@@ -277,6 +287,7 @@ const ProductSearch = ({ onProductClick }) => {
           </div>
         )}
       </div>
+
     </div>
   );
 };
