2d060743bff3de9997156304240dfb6d2470bef1
maazmasood <84343122+maazmasood@users.noreply.github.com>
2024-03-27 03:48:20 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,23 +7,35 @@ import Footer from "@/pages/components/Footer";
 import Navbar from "@/pages/components/Navbar";
 import PrivacySlider from "@/pages/components/slider";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import { PhoneIcon } from "@heroicons/react/24/outline";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <Head></Head>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-308056398"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-308056398');
+            `,
+          }}
+        />
+      </Head>
       <a
         href="tel:023694049939"
         title="Klima-Nrw-Telefon"
         className="lg:hidden fixed bottom-6 right-4 z-50 inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        <DevicePhoneMobileIcon className="h-7 w-6 " aria-hidden="true" />
-        <span className="sr-only">Notifications</span>
-        <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500  border-white rounded-full -top-2 -right-2 dark:border-gray-900">
-          1
-        </div>
+        <PhoneIcon className="h-7 w-6" aria-hidden="true" />
       </a>
       <Navbar></Navbar>
       <Component {...pageProps} />
