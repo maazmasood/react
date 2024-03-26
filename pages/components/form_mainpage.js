30fc0c67751dd6b36250423e1d32717d95919ef9
maazmasood <84343122+maazmasood@users.noreply.github.com>
2024-03-27 04:03:02 +0500

    Update form_mainpage.js

--- pages/components/form_mainpage.js
+++ pages/components/form_mainpage.js
@@ -69,7 +69,7 @@ export default function Kontakt() {
             <div className="relative">
               <input
                 type="text"
-                placeholder="Gib deinen Namen ein"
+                placeholder="Name"
                 required
                 name="full-name"
                 className="px-4 py-2 w-full border-b border-blue-600 focus:outline-none focus:border-blue-800"
